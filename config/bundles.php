c8b366c3fde9128601ce71f43574f011cccf76fd
Michał Mleczko <do@mleczki.pl>
2026-06-05 01:49:32 +0200

    Add test-pack with basic test

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     SwooleBundle\SwooleBundle\Bridge\Symfony\Bundle\SwooleBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
